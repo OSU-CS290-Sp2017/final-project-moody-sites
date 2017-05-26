41e7058a855a3698bd6172e76c959196be65a298
hugheste <hugheste@oregonstate.edu>
2017-05-26 15:02:49 -0700

    addition of upper tab

---
+++ lonely/lonely.js
@@ -0,0 +1,10 @@
+
+/*var webLink = document.getElementsByClassName("box-link")[0];
+
+webLink.addEventListener('click', openInNewTab());
+function openInNewTab() {
+  console.log("You are in openNewTab");
+  var webText = document.getAttribute('href');
+  window.open(webText, '_blank');
+  window.focus();
+}*/
