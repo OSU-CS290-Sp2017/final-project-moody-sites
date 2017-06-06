6b6b3654e043fb8ffe8e70234a36768d5f070c61
Cody Schlabach <Cody Schlabach>
2017-06-05 20:48:19 -0700

    the server

---
+++ server.js
@@ -0,0 +1,48 @@
+/*
+ * Write your Express server in this file as described in README.md.
+ */
+ var path = require('path');
+ var fs = require('fs');
+ var express = require('express');
+ var exphbs = require('express-handlebars');
+
+ var boxData = require('./boxData');
+ var app = express();
+ var port = process.env.PORT || 3000;
+
+ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+ app.set('view engine', 'handlebars');
+
+ app.get('/', function (req, res, next) {
+
+   res.render('homePage');
+ });
+
+ app.get('/moods/:mood', function (req, res, next) {
+  console.log("== url params for request:", req.params);
+  var theMood = req.params.mood;
+  var moodData = boxData[theMood];
+  if (moodData) {
+    var templateArgs = {
+      box: moodData.boxes,
+      pageName: boxData[theMood].pageName
+    }
+    res.render('moodPage', templateArgs);
+    console.log("boxData.boxes = ",boxData.boxes);
+    console.log("moodData = ",moodData);
+  } else {
+    next();
+  }
+});
+
+
+ app.use(express.static(path.join(__dirname, 'public')));
+
+ app.get('*', function (req, res) {
+   res.render('404Page');
+ });
+
+ // Start the server listening on the specified port.
+ app.listen(port, function () {
+   console.log("== Server listening on port", port);
+ });
