ce5be6d6adacdb0f6002220d1c52afdc1dd92817
Cody Schlabach <Cody Schlabach>
2017-06-06 14:37:39 -0700

    updated changes

--- public/webBoxTemplate.js
+++ public/webBoxTemplate.js
@@ -3,7 +3,9 @@
 templates['webBox'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<article class=\"mood_box\">\r\n  <div class=\"box-content\">\r\n    <p class=\"box_header\"><strong><a class=\"box-link\" href=\""
+  return "<article class=\"mood_box\" class=\""
+    + alias4(((helper = (helper = helpers.style || (depth0 != null ? depth0.style : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"style","hash":{},"data":data}) : helper)))
+    + "\">\r\n  <div class=\"box-content\">\r\n    <p class=\"box_header\"><strong><a class=\"box-link\" href=\""
     + alias4(((helper = (helper = helpers.titleLink || (depth0 != null ? depth0.titleLink : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"titleLink","hash":{},"data":data}) : helper)))
     + "\" target=\"_blank\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
