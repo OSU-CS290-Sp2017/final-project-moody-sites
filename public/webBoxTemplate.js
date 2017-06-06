32d35363d185264dd1d606c9157dcd941f9c032d
Cody Schlabach <Cody Schlabach>
2017-06-05 20:50:33 -0700

    public folder

---
+++ public/webBoxTemplate.js
@@ -0,0 +1,16 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['webBox'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<article class=\"mood_box\">\r\n  <div class=\"box-content\">\r\n    <p class=\"box_header\"><strong><a class=\"box-link\" href=\""
+    + alias4(((helper = (helper = helpers.titleLink || (depth0 != null ? depth0.titleLink : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"titleLink","hash":{},"data":data}) : helper)))
+    + "\" target=\"_blank\">"
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</a></strong></p>\r\n    <p class=\"box-text\">\r\n        "
+    + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
+    + "\r\n    </p>\r\n    <img class=\"box-image\" src=\""
+    + alias4(((helper = (helper = helpers.photoURL || (depth0 != null ? depth0.photoURL : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"photoURL","hash":{},"data":data}) : helper)))
+    + "\">\r\n  </div>\r\n</article>\r\n";
+},"useData":true});
+})();
