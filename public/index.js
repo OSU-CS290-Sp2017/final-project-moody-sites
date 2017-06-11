185a7daa59a432a54cf14e6d66931e2bc0462bd5
codyschlabach <schlabac@oregonstate.edu>
2017-06-11 16:15:44 -0700

    update index.js

--- public/index.js
+++ public/index.js
@@ -53,4 +53,108 @@ function searchFunction(){
 
 }
 
-searchButton.addEventListener('click',searchFunction);
+if(searchButton){
+  searchButton.addEventListener('click',searchFunction);
+}
+
+/*****************************add box************************/
+/**********************adding new webBox****************************/
+var allBoxes = [];
+
+function showCreateBoxModal() {
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var createBoxModal = document.getElementById('create-box-modal');
+
+  modalBackdrop.classList.remove('hidden');
+  createBoxModal.classList.remove('hidden');
+  console.log("inside showCreateBoxModal");
+}
+
+function closeCreateBoxModal() {
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var createBoxModal = document.getElementById('create-box-modal');
+
+  modalBackdrop.classList.add('hidden');
+  createBoxModal.classList.add('hidden');
+
+  clearBoxInputValues();
+}
+
+function clearBoxInputValues() {
+  var boxInput = document.getElementsByClassName('box-input-element');
+  for (var i = 0; i < boxInput.length; i++) {
+    var input = boxInput[i].querySelector('input, textarea');
+    input.value = '';
+  }
+}
+
+
+
+// function generateNewBox(Boxtitle, Boxdescription, BoxtitleLink, BoxphotoURL) {
+//   /*var boxTemplate = Handlebars.views.partials.webBox;*/
+//   var boxData = {
+//     title: Boxtitle,
+//     description: Boxdescription,
+//     titleLink: BoxtitleLink,
+//     photoURL: BoxphotoURL,
+//     style: "lonely"
+//   /*  title: boxTitle,
+//     description: boxDescription,
+//     titleLink: boxTitleLink,
+//     photoURL: boxPhotoURL,
+//     style: boxStyle*/
+//   };
+//   // res.render('webBox', templateArgs);
+// /*  return boxTemplate(boxData);*/
+// }
+
+function insertNewBox() {
+
+  var boxTitle = document.getElementById('box-title-input').value;
+  var boxDescription = document.getElementById('box-description-input').value;
+  var boxTitleLink = document.getElementById('box-link-input').value;
+  var boxPhotoURL = document.getElementById('box-photoURL-input').value;
+  /*var boxStyle = document.getElementById('lonely').value; /***********************/
+
+  if (boxTitle && boxDescription && boxTitleLink && boxPhotoURL) {
+      var newBox = generateNewBox(boxTitle, boxDescription, boxTitleLink, boxPhotoURL);
+      var boxContainer = document.querySelector('.box-container');
+      boxContainer.insertAdjacentHTML('beforehand', newBox);
+      allBoxes.push(newBox);
+
+      closeCreateBoxModal();
+  } else {
+    alert('You must specify the title, description, title link, and photo URL!');
+  }
+}
+
+
+
+
+var boxCollection = document.getElementsByClassName('mood_box');
+for (var i = 0; i < boxCollection.length; i++) {
+  allBoxes.push(boxCollection[i]);
+}
+
+
+// window.addEventListener('DOMContentLoaded', function (event) {
+
+
+  var createBoxButton = document.getElementById('create-box-button');
+if(createBoxButton){
+  createBoxButton.addEventListener('click', showCreateBoxModal);
+}
+
+  var modalCloseButton = document.querySelector('#create-box-modal .modal-close-button');
+if(modalCloseButton){
+  modalCloseButton.addEventListener('click', closeCreateBoxModal);
+}
+  var modalCancelButton = document.querySelector('#create-box-modal .modal-cancel-button');
+if(modalCancelButton){
+  modalCancelButton.addEventListener('click', closeCreateBoxModal);
+}
+  var modalAcceptButton = document.querySelector('#create-box-modal .modal-accept-button');
+if(modalAcceptButton){
+  modalAcceptButton.addEventListener('click', insertNewBox);
+}
+// });
